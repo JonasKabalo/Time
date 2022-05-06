1ed4eedb9f2840c235a5df0f72c9a0254784386f
jonaskabalo <kabalojonas44@gmail.com>
2022-05-06 11:50:31 +0100

    update new files

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const { app, BrowserWindow } = require('electron')
 const path = require('path')
 
-function createWindow () {
+const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 170,
+    height: 60,
+    // frame: false,
+    icon: path.join(__dirname, '__try.icns'),
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
@@ -25,17 +27,19 @@ function createWindow () {
 app.whenReady().then(() => {
   createWindow()
 
-  app.on('activate', function () {
+  app.on('activate', () => {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
 })
 
+// app.dock.hide()
+
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
-app.on('window-all-closed', function () {
+app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit()
 })
 
--- renderer.js
+++ renderer.js
@@ -4,3 +4,41 @@
 // `nodeIntegration` is turned off. Use `preload.js` to
 // selectively enable features needed in the rendering
 // process.
+
+const dateFormatString = (date) => {
+    const newDate = new Date(date)
+    const dayMonth = newDate.toDateString().split(' ')
+    const year = dayMonth[3].split('')
+    const fullDate = `${dayMonth[2]}-${dayMonth[1]}-${year[2]}${year[3]}`
+    return fullDate
+}
+
+const element = document.getElementById('date-time')
+if (element) element.innerText = `${dateFormatString(new Date())}`
+
+const hours = document.getElementById('date-hours')
+const minutes = document.getElementById('date-minutes')
+const seconds = document.getElementById('date-seconds')
+const hourTime = document.getElementById('date-hourTime')
+
+const updateDateTime = () => {
+    const now = new Date()
+
+    const p_hours = now.getHours()
+    hours.innerText = p_hours % 12 || 12
+    minutes.innerText = `${getZeroPad(now.getMinutes())}`
+    seconds.innerText = `${getZeroPad(now.getSeconds())}`
+    hourTime.innerText = getHourTime(hours)
+
+    setTimeout(updateDateTime, 1000)
+}
+
+const getHourTime = (h) => {
+    return h >= 12 ? 'PM' : 'AM'
+}
+
+const getZeroPad = (n) => {
+    return (parseInt(n, 10) >= 10 ? '' : '0') + n
+}
+
+updateDateTime()
